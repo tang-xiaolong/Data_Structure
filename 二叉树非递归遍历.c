37db0fe07e93483fee37df0151045a7da7486681
xiaoDragon <1097145586@qq.com>
2018-07-31 20:03:42 +0800

    补全了非递归遍历，主要有：非递归前序、非递归中序、非递归后序和层次遍历

--- 二叉树非递归遍历.c
+++ 二叉树非递归遍历.c
@@ -6,12 +6,13 @@ typedef struct node
 	char c;
 	struct node *lC,*rC;
 }Tree,*pTree;
-
 void Init(pTree);
 pTree PreGetValue(pTree);
 void PreTreaverse(pTree);
 void PrePrint(pTree);
 void MidPrint(pTree);
+void LastPrint(pTree);
+void FloorPrint(pTree);
 
 int main(void)
 {
@@ -26,6 +27,12 @@ int main(void)
 	putchar('\n');
 	printf("非递归中序输出结果：");
 	MidPrint(&tree);
+	putchar('\n');
+	printf("非递归后序输出结果：");
+	LastPrint(&tree);
+	putchar('\n');
+	printf("层次遍历输出的结果：");
+	FloorPrint(&tree);
 	putchar('\n');
 	return 0;
 }
@@ -45,7 +52,6 @@ pTree PreGetValue(pTree p)
 		p->c = c;//ABD##E##CF###
 		p->lC = (pTree)malloc(sizeof(Tree));
 		p->rC = (pTree)malloc(sizeof(Tree));
-		//printf("%x  %x  %x\n",p,p->lC,p->rC);
 		p->lC = PreGetValue(p->lC);
 		p->rC = PreGetValue(p->rC);
 	}
@@ -68,47 +74,103 @@ void PreTreaverse(pTree p)
 	}
 }
 
-
-//非递归前序输出
 void PrePrint(pTree p)
 {
 	pTree bucket[maxSize];
 	int top = -1;
-	bucket[++top] = p;
-	while(top != -1)//栈不为空
-	{//算法思路：先访问当前节点。然后右孩子不为空则将右孩子压栈，左孩子不为空则将左孩子压栈，然后出栈继续
-		p =  bucket[top--];
-		putchar(p->c);
-		if(p->rC)
+	while(p!= NULL || top != -1)
+	{
+		while(p)
 		{
-			bucket[++top] = p->rC;
+			bucket[++top]  = p;
+			printf("%c",p->c);
+			p = p->lC;
 		}
-		if(p->lC)
+		if(top != -1)
 		{
-			bucket[++top] = p->lC;
+			p = bucket[top--];
+			p = p->rC;
 		}
 	}
 }
 
-//非递归中序输出
 void MidPrint(pTree p)
 {
 	pTree bucket[maxSize];
 	int top = -1;
-	while(top != -1 || p!= NULL)//满足一个就循环
-	{//当栈为空，且最后的p是空，就退出了
+	while(p != NULL || top != -1)
+	{
 		while(p)
-		{//一路向左，把左孩子全部入栈先
+		{
 			bucket[++top] = p;
-			p = p->lC;
+			p =  p->lC;
 		}
-		//左孩子都进去了，现在输出自身，并访问右孩子
 		if(top != -1)
 		{
 			p = bucket[top--];
 			putchar(p->c);
-			p = p->rC;//访问右孩子，右孩子也一路向左
+			p =  p->rC;
 		}
 	}
 }
 
+void LastPrint(pTree p)
+{
+	pTree bucket[maxSize];
+	int top =  -1;
+	int flag[maxSize] =  {0};
+	while(p != NULL || top != -1)
+	{
+		while(p)
+		{//老规矩，一路向左，直到为空
+			bucket[++top] = p;
+			flag[top] = 0;
+			p = p->lC;
+		}
+		if(top != -1)
+		{//取出栈顶元素，这时候根据flag数组对应的值来判断要不要pop来这个元素
+			p = bucket[top];
+			if(flag[top] == 0)//如果是0，证明之前还只是访问一次，这是第二次，所以不要访问，放flag对应位置变成1，并访问右孩子
+			{
+				flag[top] = 1;
+				p = p->rC;
+			}//当碰到他且flag数组是1，证明已经访问过右孩子了，所以直接pop出来并输出值。当一个节点值输出后，必须继续pop数，所以将p其置为NULL
+			else if(flag[top] == 1)
+			{
+				putchar(p->c);
+				top--;
+				p = NULL;
+			}
+		}
+	}
+}
+
+void FloorPrint(pTree p)
+{
+	pTree queue[maxSize];
+	int n = 0,front = 0;
+	if(p)
+		queue[n++] = p;
+	else
+		return;
+	while(n != front)
+	{
+		p =  queue[front++];
+		putchar(p->c);
+		if(p->lC)
+			queue[n++] = p->lC;
+		if(p->rC)
+			queue[n++] = p->rC;
+		
+	}
+}
+//示例输入
+/*
+ABD##E##CF###
+  递归前序输出结果：ABDECF
+非递归前序输出结果：ABDECF
+非递归中序输出结果：DBEAFC
+非递归后序输出结果：DEBFCA
+层次遍历输出的结果：ABCDEF
+Press any key to continue
+*/
