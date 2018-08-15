88c86d2306f71696852f795f90ab8a3097e7b16f
xiaoDragon <1097145586@qq.com>
2018-08-15 20:56:17 +0800

    修改了用邻接表创建图的输入，自动从零递增生成顶点编号

--- 天勤第七章综合应用题1.7将图的邻接表改成邻接矩阵.c
+++ 天勤第七章综合应用题1.7将图的邻接表改成邻接矩阵.c
@@ -44,7 +44,7 @@ int main(void)
 	MGraph MG;
 	CreateAGraph(&AG);
 	Conversion(&AG,&MG);
-	DFS(&MG,1);
+	DFS(&MG,0);
 	putchar('\n');
 	return 0;
 }
@@ -58,16 +58,17 @@ void CreateAGraph(AGraph *G)
 	scanf("%d%d",&G->n,&G->e);
 	for(i = 0;i < G->n;++i)
 	{
-		scanf("%d",&G->adjList[i].data);//���붥����
-		G->adjList[i].firstArc = NULL;//������ָ��ı��ÿ�
+		G->adjList[i].data = i;
+		//scanf("%d",&G->adjList[i].data);//输入顶点标号
+		G->adjList[i].firstArc = NULL;//让他们指向的边置空
 	}
-	//������
+	//处理边
 	for(i = 0;i < G->e;++i)
 	{
 		scanf("%d%d%d",&j,&k,&w);
-		//j��kȨֵΪw
+		//j到k权值为w
 		p = (ArcNode*)malloc(sizeof(ArcNode));
-		p->w = w;//Ȩֵ
+		p->w = w;//权值
 		p->adjVex = k;
 		p->nextArc = G->adjList[j].firstArc;
 		G->adjList[j].firstArc = p;
@@ -77,21 +78,21 @@ void CreateAGraph(AGraph *G)
 void CreateMGraph(MGraph *G)
 {
 	int i,j,k,w;
-	scanf("%d%d",&G->n,&G->e);//����G�ı����͵���
-	//��ʼ���ڽӾ���
+	scanf("%d%d",&G->n,&G->e);//读入G的边数和点数
+	//初始化邻接矩阵
 	for(i = 0;i < G->n;++i)
 		for(j = 0;j < G->n;++j)
 		{
 			G->edges[i][j] = Inf;
 		}
-	//ѭ������ÿ�������Ϣ
+	//循环读入每个点的信息
 	for(i = 0;i < G->n;++i)
 	{
 		scanf("%d",&G->vex[i].data);
 	}
-	//ѭ������ÿ����
+	//循环读入每条边
 	for(i = 0;i < G->e;++i)
-	{//�����������Լ����ǵ�Ȩֵ
+	{//读入两条边以及他们的权值
 		scanf("%d%d%d",&j,&k,&w);
 		G->edges[j][k] = w;
 		G->edges[k][j] = w;
@@ -100,14 +101,14 @@ void CreateMGraph(MGraph *G)
 
 void Conversion(AGraph *AG,MGraph *MG)
 {
-	//��һ�����ڽӱ���ʽ�洢��ͼ���ڽӾ����
+	//将一个以邻接表方式存储的图用邻接矩阵存
 	int i = 0,k;
 	ArcNode *p;
 	MG->n = AG->n;
 	MG->e = AG->e;
 	for(i = 0;i < AG->n;++i)
 		MG->vex[i].data = AG->adjList[i].data;
-	for(i = 0;i < MG->n;++i)//��ʼ��Ȩֵ����
+	for(i = 0;i < MG->n;++i)//初始化权值数组
 		for(k = 0;k < MG->n;++k)
 			MG->edges[i][k] = Inf;
 	for(i = 0;i < AG->n;++i)
@@ -115,8 +116,8 @@ void Conversion(AGraph *AG,MGraph *MG)
 		p = AG->adjList[i].firstArc;
 		while(p)
 		{
-			k = p->adjVex;//ȡ�ö�����
-			MG->edges[i][k] = p->w;//ȡ��Ȩֵ
+			k = p->adjVex;//取得顶点编号
+			MG->edges[i][k] = p->w;//取得权值
 			p = p->nextArc;
 		}
 	}
@@ -144,18 +145,12 @@ void DFS(MGraph *MG,int v)
 }
 
 /*
-7 12
-0 1 2 3 4 5 6
-0 1 4
-0 2 6
-0 3 6
-1 2 1
-1 4 7
-2 4 6
-2 5 4
-3 2 2
-3 5 5
-4 6 6
-5 4 1
-5 6 8
+9 14
+0 1 10 0 5 11
+1 2 18 1 6 16 1 8 12
+2 3 22 2 8 8
+3 4 20 3 7 16 3 8 21
+4 5 26 4 7 7
+5 6 17
+6 7 19
 */
