bf4679cb2c5531a1846313bb1d2de7af436c7892
xiaoDragon <1097145586@qq.com>
2018-09-02 22:24:05 +0800

    Rename 2.3.31——双向循环链表非递减排序.c to 1800/2.3.31——双向循环链表非递减排序.c

--- 1800/2.3.31——双向循环链表非递减排序.c
+++ 1800/2.3.31——双向循环链表非递减排序.c
@@ -1,4 +1,4 @@
-//2018��7��9�� 20:31:35
+//2018年7月9日 20:31:35
 #include<stdio.h>
 #include<malloc.h>
 typedef struct node
@@ -19,9 +19,9 @@ int main(void)
 	BNODETP * pHead = NULL;
 	pHead = Init();
 	Fuzhi(pHead);
-	Treaverse(pHead,"δ���������Ϊ��");
+	Treaverse(pHead,"未排序的链表为：");
 	unknown(pHead);
-	Treaverse(pHead,"����������Ϊ��");
+	Treaverse(pHead,"排序后的链表为：");
 	return 0;
 }
 
@@ -38,7 +38,7 @@ void Fuzhi(BNODETP* pHead)
 {
 	int value = 0;
 	BNODETP *p = pHead,*q;
-	printf("������������Ҫ��ȡ�����ݣ�����32768��������");
+	printf("请输入您链表要存取的数据（输入32768结束）：");
 	scanf("%d",&value);
 	while(value!=32768)
 	{
@@ -78,15 +78,15 @@ void unknown(BNODETP *L)
 		while((p!=L) && (p->data>q->data))
 		{
 			p = p->prior;
-		}//���ǲ������򣬴�β����ͷ�����Һ��ʵ�λ��
-		q->prior->next = r;//�ȰѴ�����Ľڵ��ǰһ���ڵ����������ĺ�һ���ڵ�
-		r->prior = q->prior;//1���Ѵ�����ڵ�ĺ�һ���ڵ�ǰָ����ָ���Լ���ǰһ���ڵ�
-		q->next = p->next;//���Լ��ĺ�ָ����ָ��Ҫ�����λ���ϵĽڵ�ĺ�һ���ڵ㣬
-		q->prior = p;//���Լ�ǰָ����ָ��Ҫ�����λ���ϵĽڵ�
-		p->next->prior=q;//2����Ҫ�����λ���ϵĽڵ��һ���ڵ�ǰָ����ָ���Լ�
-		p->next=q;//3����Ҫ�����λ���ϵĽڵ��ָ����ָ���Լ�
+		}//还是插入排序，从尾部往头部查找合适的位置
+		q->prior->next = r;//先把待插入的节点的前一个节点链接上它的后一个节点
+		r->prior = q->prior;//1、把待插入节点的后一个节点前指针域指向自己的前一个节点
+		q->next = p->next;//将自己的后指针域指向要插入的位置上的节点的后一个节点，
+		q->prior = p;//让自己前指针域指向要插入的位置上的节点
+		p->next->prior=q;//2、把要插入的位置上的节点后一个节点前指针域指向自己
+		p->next=q;//3、把要插入的位置上的节点后指针域指向自己
 		q = r;
 		p = q->prior;
-		r = q->next;//4����rָ��q�ĺ���һ���ڵ�
+		r = q->next;//4、把r指向q的后面一个节点
 	}
 }
