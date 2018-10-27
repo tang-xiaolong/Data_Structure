5861d72b2522529b045fe9b43acb0258abe6485a
xiaoDragon <1097145586@qq.com>
2018-10-27 12:09:54 +0800

    Update 判断标准形式的二叉树是否是堆.c
    修复了一个问题，之前只是单纯从值上判断是否满足，现在还加入了判断左右子树节点数目是否满足条件

--- 判断标准形式的二叉树是否是堆.c
+++ 判断标准形式的二叉树是否是堆.c
@@ -11,30 +11,37 @@ typedef struct node
 }Node;
 
 Node *PreCreate(Node *p);
-int estimate_stack(Node *t);
+int estimate_stack(Node *t,int *n);
 #include<stdio.h>
 int main(void)
 {
 	Node tree;
+	int n = 0;
 	//freopen("Input.txt","r",stdin);
 	PreCreate(&tree);//按照先序输入一棵树，值为32767表示这个节点是空节点
-	if(estimate_stack(&tree))
-		printf("这是一个堆！\n");
+	if(estimate_stack(&tree,&n))
+		printf("这是一个堆！节点个数为：%d\n",n);
 	else
-		printf("这不是一个堆！\n");
+		printf("这不是一个堆！节点个数为：%d\n",n);
 	return 0;
 }
 
-int estimate_stack(Node *t)
+int estimate_stack(Node *t,int *n)
 {
 	int a = 1,b = 1;//标志位
+	int n1 = 0,n2 = 0;
 	if(t)
 	{
 		if(t->lc)//如果存在左子树，先判断值是否满足条件，如果满足再递归判断左子树是否满足条件。将最终的结果&&起来
-			a = (t->data BIG t->lc->data) && estimate_stack(t->lc);
+			a = (t->data BIG t->lc->data) && estimate_stack(t->lc,&n1);
 		if(t->rc && a)//如果左子树已经不满足了，不用继续递归右子树了
-			b = (t->data BIG t->rc->data) && estimate_stack(t->rc);
+			b = (t->data BIG t->rc->data) && estimate_stack(t->rc,&n2);
+		if(n1 < n2)//如果左子树个数比右子树个数少，则不是
+			a = b = 0;
+		*n = n1 + n2 + 1;//左右子树加1为自己当前的节点个数
 	}
+	else
+		*n = 0;
 	return a && b;//如果为空，则表示这个节点满足定义
 }
 Node *PreCreate(Node *p)
@@ -56,4 +63,6 @@ Node *PreCreate(Node *p)
 45 32 17 20 32767 32767 10 32767 32767 14 11 32767 32767 12 32767 32767 32767
 输入样例2：
 45 32 17 14 32767 32767 10 32767 32767 20 11 32767 32767 12 32767 32767 32767
+输入样例3：
+45 32 32767 32767 20 12 32767 32767 14 32767 32767
 */
