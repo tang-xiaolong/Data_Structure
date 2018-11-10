534402f292c0b04207e531bd183400f1aaddad63
xiaoDragon <1097145586@qq.com>
2018-11-10 20:14:13 +0800

    Update and rename 常见的几种排序方法及思想.c to 排序--常见的几种排序方法及思想.c

--- 排序--常见的几种排序方法及思想.c
+++ 排序--常见的几种排序方法及思想.c
@@ -1,21 +1,21 @@
-//ð������  һ��һ���������α�֤ǰ����ʣ�µ���������
-//��������  ͨ��ĳ�ַ��������ν�����һ�����ݲ嵽ǰ�����ݵ�ĳ��λ�ã���֤ǰ������������ġ�
-//ѡ������  ͨ��ĳ�ַ������Ӻ����������ѡ��һ�����������͵�һ�����������ٴӺ���������У�ѡ��һ������������ڶ�����������
-//��������
-//�鲢����  ������������  ���ĸ��ĸ���  �ٰ˸��˸���
+//冒泡排序  一轮一轮来,每次比较相邻的两个，依次保证最后的是剩下的里面最大的
+//插入排序  通过某种方法，依次将后面一个数据插到前面数据的某个位置，保证前面数据是有序的。
+//选择排序  通过某种方法，从后面的数据中选择一个最大的数，和第一个数互换。再从后面的数据中，选择一个最大的数，与第二个数互换。
+//快速排序
+//归并排序  先两个两个排  再四个四个排  再八个八个排
 #include<stdio.h>
 
-//��������
+//快速排序
 void QuickSort(int* ,int ,int );
 int FindPos(int* ,int ,int );
 
-//ð������
+//冒泡排序
 void MaoPaoSort(int *,int );
 
-//��������
+//插入排序
 void InsertSort(int* ,int);
 
-//ѡ������
+//选择排序
 void SelectSort(int*,int);
 int main(void)
 {
@@ -39,7 +39,7 @@ void QuickSort(int* a,int low,int high)
 	int pos;
 	if(low < high)
 	{
-		//�ҵ�һ��λ�ã���ߵİ�ͬ�����㷨�ţ��ұ�Ҳ��ͬ�����㷨��
+		//找到一个位置，左边的按同样的算法排，右边也按同样的算法排
 		pos = FindPos(a,low,high);
 		QuickSort(a,low,pos-1);
 		QuickSort(a,pos+1,high);
@@ -57,7 +57,7 @@ int FindPos(int* a,int low,int high)
 			low++;
 		a[high] = a[low];
 	}
-	//�����λ���Ѿ����ˡ�
+	//到这里，位置已经定了。
 	a[low] = val;
 	return low;
 }
@@ -67,15 +67,15 @@ void MaoPaoSort(int* a,int len)
 {
 	int val;
 	int i,j;
-	for(i = 0;i < len-1;i++)
+	for(i = len - 1;i > 0;--i)
 	{
-		for(j = i+1;j < len;j++)
+		for(j = 0;j < i;++j)
 		{
-			if(a[i] > a[j])
+			if(a[j] > a[j + 1])
 			{
-				val = a[i];
-				a[i] = a[j];
-				a[j] = val;
+				val = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = val;
 			}
 		}
 	}
@@ -105,7 +105,7 @@ void SelectSort(int* a,int len)
 	{
 		for(j = index = i;j < len-1;j++)
 		{
-			//�ҵ�ʣ�µ��������(����С)�����±�
+			//找到剩下的里面最大(或最小)的数下标
 			if(a[j+1] < a[index])
 				index = j+1;
 		}
